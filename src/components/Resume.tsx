ebea44ceeccc16ed1997c54a330d808be5519398
phayvour1 <pharlorlah700@gmail.com>
2024-11-29 22:38:55 +0100

    resume update

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,27 +1,16 @@
 
-import { useNavigate } from "react-router-dom";
+
 
 const Resume = () => {
-  const navigate = useNavigate();
   const resumeViewUrl = "https://drive.google.com/file/d/13o_vKWFnRPAsCZdm5AaNmbPAZSvQQVmS/preview"; // Viewable Google Drive link
-  const resumeDownloadUrl = "https://drive.google.com/uc?export=download&id=13o_vKWFnRPAsCZdm5AaNmbPAZSvQQVmS"; // Direct download link
-
-  // Navigate to the homepage
-  const goToHome = () => {
-    navigate("/"); // Go back to the homepage
-  };
+  
+  
 
  
 
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center relative">
-      {/* Home Button */}
-      <button
-        onClick={goToHome}
-        className="absolute top-4 left-4 px-6 py-3 bg-gray-500 text-white rounded-lg hover:bg-gray-400 transition-all"
-      >
-        Home
-      </button>
+     
 
       {/* PDF Viewer */}
       <div className="w-full  h-screen">
@@ -32,14 +21,6 @@ const Resume = () => {
         ></iframe>
       </div>
 
-      {/* Download Button */}
-      <a
-        href={resumeDownloadUrl}
-        download
-        className=" absolute bottom-7 right-4 transform -translate-x-1/2 px-2 py-1 bg-gray-500 text-white rounded-lg hover:bg-gray-900 transition-all"
-      >
-        Download 
-      </a>
     </div>
   );
 };
